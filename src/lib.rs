e34ce4f66505a4b0825d7b8e084f6ba9c016cdf6
Anthony Delannoy <anthony.delannoy@softathome.com>
2021-10-17 20:35:31 +0200

    lib.rs: first draft

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+mod pressure;
+pub use crate::pressure::*;
+
+pub mod sys;
+pub use crate::sys::kernel::osrelease::*;
+
+pub mod uptime;
+pub use crate::uptime::*;
+
+pub mod stat;
+pub use crate::stat::*;
